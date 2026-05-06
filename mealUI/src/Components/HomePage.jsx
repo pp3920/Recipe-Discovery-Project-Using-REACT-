4c4e582e9e2de4a0e7fdade58ec91df6bcc4a138
Priyanka22324 <priyanka22324@gmail.com>
2026-05-05 21:20:49 -0400

    adding files

--- mealUI/src/Components/HomePage.jsx
+++ mealUI/src/Components/HomePage.jsx
@@ -1,19 +1,33 @@
 
+import React , {useState} from 'react'
 /*
 Search Bar 
 */
 
 const HomePage = () => {
+
+    {/* Storing  meal array into the data */}
+    const [data , setData] = useState()
+
+
+
+
     
-    {/*Function for search of Meal */}
+    {/*Function for search of Meal from the API */}
 
     const FunSearchMeal =  async() =>
     {
         const get = await fetch (`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`);
         const jsonData = await get.json();  {/* waiting till it gets converting it into json format */ } 
-        console.log(jsonData)
+        console.log(jsonData.meals)  
     }
     
+    {/*Function for search of Meal on Click */}
+
+    const searchValue = () =>
+    {
+        
+    }
     
     
     
@@ -24,7 +38,7 @@ const HomePage = () => {
              {/* creating search bar */}
             <div className = 'container'>
                 <div className = 'searchBar'>
-                    <input type = 'text' placeholder='Enter Dish'/>
+                    <input type = 'text' placeholder='Enter Dish'  onChange={searchValue}/>
                     <button onClick = {FunSearchMeal}>Search</button>
 
                 </div>
